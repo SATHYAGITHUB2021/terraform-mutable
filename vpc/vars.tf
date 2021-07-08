13e75c226ecf50738cdb7fa7ac0e3091e922997a
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-07-09 00:02:07 +0530

    added content makefile/immutable

--- vpc/vars.tf
+++ vpc/vars.tf
@@ -1,6 +1,6 @@
 variable "VPC_CIDR"             {}
 variable "ENV"                  {}
-variable "SUBNET_ZONES"         {}
-variable "PUBLIC_SUBNETS_CIDR"  {}
-variable "PRIVATE_SUBNETS_CIDR" {}
-variable "DEFAULT_VPC_ID"       {}
+//variable "SUBNET_ZONES"         {}
+//variable "PUBLIC_SUBNETS_CIDR"  {}
+//variable "PRIVATE_SUBNETS_CIDR" {}
+//variable "DEFAULT_VPC_ID"       {}
