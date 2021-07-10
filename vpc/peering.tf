a6cc59af831ae8517061485f2761c2412beed366
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-07-10 09:33:59 +0530

    added content for peering connection creation with auto accept /mutable-terraform

--- vpc/peering.tf
+++ vpc/peering.tf
@@ -1,4 +1,7 @@
 resource "aws_vpc_peering_connection" "peer-connection" {
   peer_vpc_id   = aws_vpc.main.id
   vpc_id        = var.DEFAULT_VPC_ID
+  auto_accept   = true
 }
+
+#auto accept is only possible if two vpc's are in same aws account.
