c9d4b2a2f17968e88460c268374acc82cdd944d4
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-07-11 15:10:21 +0530

    added content in data.tf and mongodb.tf

--- databases/mongodb.tf
+++ databases/mongodb.tf
@@ -1,11 +1,43 @@
 //resource "aws_spot_instance_request" "cheap_worker" {
-//  count                  = length(var.COMPONENTS)
-//  ami                    = "ami-059e6ca6474628ef0"
+//  ami                    = data.aws_ami.centos7.id
 //  spot_price             = "0.0031"
 //  instance_type          = "t3.micro"
 //  vpc_security_group_ids = ["sg-05150ce557f1cc782"]
 //
 //  tags = {
 //    Name = element(var.COMPONENTS, count.index)
+//  }
+//}
+//
+//resource "aws_security_group" "allow_mongodb" {
+//  name        = "allow_mongodg"
+//  description = "AllowMongoDB"
+//  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+//
+//  ingress {
+//    description      = "SSH"
+//    from_port        = 22
+//    to_port          = 22
+//    protocol         = "tcp"
+//    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+//  }
+//  ingress {
+//    description      = "MONGODB"
+//    from_port        = 27017
+//    to_port          = 27017
+//    protocol         = "tcp"
+//    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
+//  }
+//
+//  egress {
+//    from_port        = 0
+//    to_port          = 0
+//    protocol         = "-1"
+//    cidr_blocks      = ["0.0.0.0/0"]
+//    ipv6_cidr_blocks = ["::/0"]
+//  }
+//
+//  tags = {
+//    Name = "AllowMongoDB"
 //  }
 //}
