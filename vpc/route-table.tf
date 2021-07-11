bf7b39609d99a770fb9515f7338025d5d949fe4f
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-07-11 09:03:19 +0530

    added content for creation of route-table.tf /mutable-terraform

--- vpc/route-table.tf
+++ vpc/route-table.tf
@@ -3,23 +3,35 @@ resource "aws_route_table" "public-rt" {
 
   route {
     vpc_peering_connection_id = aws_vpc_peering_connection.peer-connection.id
-    cidr_block = var.DEFAULT_VPC_CIDR
+    cidr_block                = var.DEFAULT_VPC_CIDR
   }
 
   tags = {
-    Name = "public-route-table"
+    Name                      = "public-route-table"
   }
 }
 
 resource "aws_route_table" "private-rt" {
-  vpc_id = aws_vpc.main.id
+  vpc_id                      = aws_vpc.main.id
 
   route {
     vpc_peering_connection_id = aws_vpc_peering_connection.peer-connection.id
-    cidr_block = var.DEFAULT_VPC_CIDR
+    cidr_block                = var.DEFAULT_VPC_CIDR
   }
 
   tags = {
-    Name = "private-route-table"
+    Name                      = "private-route-table"
   }
 }
+
+resource "aws_route" "r" {
+  route_table_id            = var.DEFAULT_VPC_ROUTE_TABLE
+  destination_cidr_block    = var.VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer-connection.id
+}
+
+//resource "aws_route_table_association" "public-association" {
+//  count                       = length(var.SUBNET_ZONES)
+//  subnet_id                   = var.
+//  route_table_id              = aws_route_table.bar.id
+//}
