e3de7085a70c7df4a0b9b01222a27d384c34188e
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-07-11 21:09:18 +0530

    added content in data.tf and mongodb.tf

--- databases/data.tf
+++ databases/data.tf
@@ -4,12 +4,16 @@ data "aws_ami" "centos7" {
   owners           = ["973714476881"]
 }
 
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-
-  config = {
-    bucket         = "sathya-aws-bucket"
-    key            = "mutable/vpc/${var.ENV}/terraform.tfstate"
-    region         = "us-east-1"
-  }
+output "ami" {
+  value = data.aws_ami.centos7
 }
+
+//data "terraform_remote_state" "vpc" {
+//  backend = "s3"
+//
+//  config = {
+//    bucket         = "sathya-aws-bucket"
+//    key            = "mutable/vpc/${var.ENV}/terraform.tfstate"
+//    region         = "us-east-1"
+//  }
+//}
