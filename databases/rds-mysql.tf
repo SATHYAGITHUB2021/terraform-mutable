d6ddfd6dcf049873e756abf5f3c4f05a16110054
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-07-12 19:46:06 +0530

    added content in data.tf and mongodb.tf

--- databases/rds-mysql.tf
+++ databases/rds-mysql.tf
@@ -8,4 +8,40 @@
 //  password                    = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_PASS"]
 //  parameter_group_name        = "default.mysql5.7"
 //  skip_final_snapshot         = true
+//  db_subnet_group_name        = ""
+//  security_group_names        = []
+//}
+//
+//resource "aws_db_subnet_group" "mysql-db-group-${var.ENV}" {
+//  name                       = "main"
+//  subnet_ids                 = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS
+//
+//  tags = {
+//    Name                     = "mysql-db-group-${var.ENV}"
+//  }
+//}
+//resource "aws_security_group" "allow_rds_mysql" {
+//  name                   = "allow_rds_mysql"
+//  description            = "AllowRdsMySQL"
+//  vpc_id                 = data.terraform_remote_state.vpc.outputs.VPC_ID
+//
+//  ingress {
+//    description          = "MYSQL"
+//    from_port            = 3306
+//    to_port              = 3306
+//    protocol             = "tcp"
+//    cidr_blocks          = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
+//  }
+//
+//  egress {
+//    from_port           = 0
+//    to_port             = 0
+//    protocol            = "-1"
+//    cidr_blocks         = ["0.0.0.0/0"]
+//    ipv6_cidr_blocks    = ["::/0"]
+//  }
+//
+//  tags = {
+//    Name                = "AllowRdsMySQL"
+//  }
 //}
