553f83029273e5e67ce63d5b7d76f6e2eb00f25b
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-07-14 09:19:23 +0530

    added content in elastic-cache.tf

--- databases/elastic-cache.tf
+++ databases/elastic-cache.tf
@@ -7,7 +7,7 @@ resource "aws_elasticache_cluster" "example" {
   engine_version             = "5.0.6"
   port                       = 6379
   security_group_ids         = [aws_security_group.allow_elastic_redis.id]
-  security_group_names       = aws_elasticache_subnet_group.redis-subnet-group.name
+  subnet_group_name          = aws_elasticache_subnet_group.redis-subnet-group.name
 }
 
 resource "aws_elasticache_subnet_group" "redis-subnet-group" {
