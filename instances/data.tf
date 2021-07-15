9a7e94299032e12bc6aefc7f011a06ad9901d88a
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-07-15 21:18:33 +0530

    added content in instances folder

---
+++ instances/data.tf
@@ -0,0 +1,15 @@
+data "aws_ami" "centos7" {
+  most_recent      = true
+  name_regex       = "^Centos-7-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket         = "sathya-aws-bucket"
+    key            = "mutable/vpc/${var.ENV}/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
